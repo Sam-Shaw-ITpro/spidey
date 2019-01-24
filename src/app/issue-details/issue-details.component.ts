92079d026e2a736c3b6c09c49ae947a2b65ba953
Sam Shaw <sam.shaw.itpro@gmail.com>
2019-01-24 14:34:14 -0500

    removed a button

--- src/app/issue-details/issue-details.component.ts
+++ src/app/issue-details/issue-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MarvelService } from '../marvel.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import * as moment from 'moment';
 
 @Component({
@@ -22,7 +24,7 @@ export class IssueDetailsComponent implements OnInit {
     this.theYear = moment(new Date()).format('YYYY');
     this.activatedRoute.params.subscribe(params => {
       var uri = params['uri'];
-      console.log('in details ', uri)
+      // console.log('in details ', uri)
       this.getOneSpidermanComic(uri)
     },
       error => {
@@ -35,7 +37,7 @@ export class IssueDetailsComponent implements OnInit {
           this.marvelservice.getOneSpidermanComicMethod(uri)
             .subscribe(OneComic => {
               this.onecomic = OneComic.data.results[0];
-              console.log('One Comic ', this.onecomic);
+              // console.log('One Comic ', this.onecomic);
             },
               error => {
                 this.error = error;
